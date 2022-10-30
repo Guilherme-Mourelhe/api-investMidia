1ef0afcd3da481bb65c588e54211ed890a068cf8
luisguilherme <guilhermemourelhe@gmail.com>
2022-10-30 00:42:27 -0300

    trocando a chave

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -2,7 +2,7 @@ async function teste(request, response) {
   let simbolo = request.body.simbol;
 
   const viaCepResponse = await fetch(
-    `https://api.hgbrasil.com/finance/stock_price?key=a97842bb&symbol=${simbolo}`
+    `https://api.hgbrasil.com/finance/stock_price?key=3a362b98&symbol=${simbolo}`
   );
   const viaCepResponseJson = await viaCepResponse.json();
 
