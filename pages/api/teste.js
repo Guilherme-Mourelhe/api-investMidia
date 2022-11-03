9aa3262d76ec2b69f88b492cd57a77faa8666d04
luisguilherme <guilhermemourelhe@gmail.com>
2022-11-03 18:36:48 -0300

    testefinal

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -2,7 +2,7 @@ async function teste(request, response) {
   let simbolo = request.body.simbol;
 
   const viaCepResponse = await fetch(
-    `https://api.hgbrasil.com/finance/stock_price?key=10b5bed5&symbol=${simbolo}`
+    `https://api.hgbrasil.com/finance/stock_price?key=330d3dad&symbol=${simbolo}`
   );
   const viaCepResponseJson = await viaCepResponse.json();
 
