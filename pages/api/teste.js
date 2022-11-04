3e5a92750bf374b85d735ff62a58d45f54dd6f38
luisguilherme <guilhermemourelhe@gmail.com>
2022-11-04 17:09:15 -0300

    thenewapi

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -2,8 +2,8 @@ async function teste(request, response) {
   let simbolo = request.body.simbol;
 
   const viaCepResponse = await fetch(
-    `https://cotacao.b3.com.br/mds/api/v1/InstrumentQuotation/${simbolo}`
-    //`https://api.hgbrasil.com/finance/stock_price?key=330d3dad&symbol=${simbolo}`
+    //`https://cotacao.b3.com.br/mds/api/v1/InstrumentQuotation/${simbolo}`
+    `https://api.hgbrasil.com/finance/stock_price?key=330d3dad&symbol=${simbolo}`
   );
   const viaCepResponseJson = await viaCepResponse.json();
 
